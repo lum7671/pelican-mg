3ac80134f122a05b1a8e4f3d91957face26fd72f
Nate Doohyun Jang <x@Nateui-iMac.local>
2017-04-13 23:51:53 +0900

    ..

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,1 @@
-'use strict';
-
-$(document).ready(function() {
-    $('.mg-container-social').height($('article').height());
-    $('#mg-panel-social').stick_in_parent({offset_top: 35});
-});
+"use strict";$(document).ready(function(){$(".mg-container-social").height($("article").height()),$("#mg-panel-social").stick_in_parent({offset_top:35})});
